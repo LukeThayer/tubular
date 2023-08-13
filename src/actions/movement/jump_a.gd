63cd453e66b22802dc4633ee9b7dc1ca3940cf20
Luke Thayer <luke@butmobile.local>
2023-08-13 14:38:43 -0700

    update

--- src/actions/movement/jump_a.gd
+++ src/actions/movement/jump_a.gd
@@ -1,6 +1,16 @@
+
 extends Node
+var parent: Node = null
+
+func _ready():
+	parent = self.get_parent()
+
+
 func start_action(aput:Aput):
+	parent.apply_jump()
 	pass
 
 func end_action():
 	pass
+
+
--- src/actions/movement/move_a.gd
+++ src/actions/movement/move_a.gd
@@ -1,20 +1,16 @@
 extends Node
-var nodeToMove: CharacterBody2D = null
+var parent: Node = null
+
+func _ready():
+	parent = self.get_parent()
+
+
 
 func start_action(aput:Aput):
-	dir = aput.direction
+	parent.CurrentDirection = aput.direction
 	pass
 
 func end_action():
+	parent.CurrentDirection = Vector2(0,0)
 	pass
 
-var dir : Vector2 = Vector2(0,0)
-
-func _physics_process(delta):
-	nodeToMove.velocity = dir * 1000
-	nodeToMove.move_and_slide()
-
-
-func init(node:CharacterBody2D):
-	nodeToMove = node
-	pass
